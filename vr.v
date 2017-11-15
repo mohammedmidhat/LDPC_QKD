6f62b29e20915d1d1c3a044a053bf2f249ef5cb3
mohammed <mohammed>
2017-11-15 10:06:13 -0500

    HDL auto generation Python script for the brute force decoder is added

--- vr.v
+++ vr.v
@@ -1,13 +1,16 @@
-module vr#(parameter DATA_WIDTH = 6)(
-	input [DATA_WIDTH-1:0] ch_msg_0,
-	input [DATA_WIDTH-1:0] ch_msg_1,
-	input [DATA_WIDTH-1:0] ch_msg_2,
-	output [DATA_WIDTH-1:0] vr_msg_0,
-	output [DATA_WIDTH-1:0] vr_msg_1,
-	output [DATA_WIDTH-1:0] vr_msg_2,
-	output [DATA_WIDTH-1:0] belief
+module vr#(parameter INT = 8, parameter FRAC = 8)(
+	input [INT+FRAC-1:0] ch_msg_0,
+	input [INT+FRAC-1:0] ch_msg_1,
+	input [INT+FRAC-1:0] ch_msg_2,
+	output [INT+FRAC-1:0] vr_msg_0,
+	output [INT+FRAC-1:0] vr_msg_1,
+	output [INT+FRAC-1:0] vr_msg_2,
+	output [INT+FRAC-1:0] belief
 	);
 
 assign belief = vr_msg_0 + vr_msg_0 + vr_msg_0;
+assign vr_msg_0 = vr_msg_1 + vr_msg_2;
+assign vr_msg_1 = vr_msg_0 + vr_msg_2;
+assign vr_msg_2 = vr_msg_0 + vr_msg_1;
 
 endmodule
